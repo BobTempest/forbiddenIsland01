655674034679dbef63ce42e615a36370015d1446
vincenal <vincenal@microsoft.com>
2018-04-06 10:07:46 +0200

    player pawns OK

--- src/index.js
+++ src/index.js
@@ -2,6 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const playerColors = { "Red" : "#CC0000", "Yellow" : "#FFF200", "White" : "#FFFFFF", "Black" : "#000000", "Green" : "#0AB300", "Blue" : "#0064b3"};
+
+const orthogonalPaths =  {0 : [1,3], 1 : [0,4], 2 : [3,7], 3 : [0,2,4,8], 4 : [1,3,5,9], 5 : [4,10],
+  6 : [7,12], 7 : [2,6,8,13], 8 : [3,7,9,14], 9 : [4,8,10,15], 10 : [5,9,11,16], 11 : [10,17], 12 : [6,13], 13 : [7,12,14,18],
+  14 : [8,13,15,19], 15 : [9,14,16,20], 16 : [10,15,17,21], 17 : [11,16], 18 : [13,19], 19 : [14,18,20,22], 20 : [15,19,21,23],
+  21 : [16,20], 22 : [19,23], 23 : [20,22]};
+const diagonalPaths = {0 : [4], 1 : [3], 2 : [6,8], 3 : [1,7,9], 4 : [0,8,10], 5 : [1,9,11], 6 : [2,13], 7 : [3,12,14],
+   8 : [2,4,13,15], 9 : [3,5,14,16], 10 : [4,15,17], 11 : [5,16], 12 : [7,18], 13 : [6,8,19],
+   14 : [7,9,18,20], 15 : [8,10,19,21], 16 : [9,11,20], 17 : [10,21], 18 : [14,22], 19 : [13,15,23], 20 : [14,16,22],
+   21 : [15,17,23], 22 : [18,20], 23 : [19,21]};
+const scubaDiversPaths = {0 : [8], 1 : [9], 2 : [4,13], 3 : [5,14], 4 : [2,15], 5 : [3,16], 6 : [8], 7 : [9,18], 8 : [0,6,11,19],
+   9 : [1,7,11,20], 10 : [8,21], 11 : [9], 12 : [14], 13 : [2,15], 14 : [3,12,16,22], 15 : [4,13,17,23], 16 : [5,14], 17 : [15],
+   18 : [7,20], 19 : [8,21], 20 : [9,18], 21 : [10,19], 22 : [14], 23 : [15]};
+
+ const gameSteps = ["init", "startTurn", "playerActionOne", "playerActionTwo", "playerActionThree", "playerPickACard", "floodRise", "endTurn", "final"];
+
+ const playerCardsLeap = [
+ "crystal","crystal","crystal","crystal","crystal",
+ "cup","cup","cup","cup","cup",
+ "statue","statue","statue","statue","statue",
+ "sceptre","sceptre","sceptre","sceptre","sceptre",
+ "helicopter","helicopter","helicopter",
+ "sandBag","sandBag",
+ "floodRise","floodRise","floodRise"];
+
+ const tilesCards = [
+ "helipad",
+ "doorBlack","doorYellow","doorGreen","doorRed","doorWhite",
+ "temple0101","temple0102","temple0201","temple0202",
+ "temple0301","templ0302","temple0401","temple0402",
+ "desert01","desert02","desert03","coast01","coast02",
+ "coast03","swamp01","swamp02","swamp03","swamp04"];
+
 function Square(props) {
   return (
     <div className="square" style={{background: props.tile.backgroundColor}} onClick={props.onClick}>
@@ -20,9 +53,27 @@ function EmptySquare() {
 }
 
 function PlayerPawn(props){
-  if (props.pawns && props.pawns.length > 0){
+  if (props.pawns && props.pawns.length === 1){
+    // let color01 = playerColors[props.pawns[0]];
     return (
-      <div className="playerPawn">P</div>
+      <div className="playerPawn singlePP" style={{color: playerColors[props.pawns[0]]}}>P</div>
+    );
+  }
+  else if(props.pawns && props.pawns.length === 2){
+    return (
+      <div className="playerPawn twoPP"><span style={{color: playerColors[props.pawns[0]]}}>P</span>&nbsp;<span style={{color: playerColors[props.pawns[1]]}}>P</span></div>
+    );
+  }
+  else if(props.pawns && props.pawns.length === 3){
+    return (
+      <div className="playerPawn multilinePP"><span style={{color: playerColors[props.pawns[0]]}}>P</span>&nbsp;<span style={{color: playerColors[props.pawns[1]]}}>P</span><br/>
+      <span style={{color: playerColors[props.pawns[2]]}}>P</span></div>
+    );
+  }
+  else if(props.pawns && props.pawns.length === 4){
+    return (
+      <div className="playerPawn multilinePP"><span style={{color: playerColors[props.pawns[0]]}}>P</span>&nbsp;<span style={{color: playerColors[props.pawns[1]]}}>P</span><br/>
+      <span style={{color: playerColors[props.pawns[2]]}}>P</span>&nbsp;<span style={{color: playerColors[props.pawns[3]]}}>P</span></div>
     );
   }
   return null
@@ -31,36 +82,6 @@ function PlayerPawn(props){
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    const orthogonalPaths =  {0 : [1,3], 1 : [0,4], 2 : [3,7], 3 : [0,2,4,8], 4 : [1,3,5,9], 5 : [4,10],
-      6 : [7,12], 7 : [2,6,8,13], 8 : [3,7,9,14], 9 : [4,8,10,15], 10 : [5,9,11,16], 11 : [10,17], 12 : [6,13], 13 : [7,12,14,18],
-      14 : [8,13,15,19], 15 : [9,14,16,20], 16 : [10,15,17,21], 17 : [11,16], 18 : [13,19], 19 : [14,18,20,22], 20 : [15,19,21,23],
-      21 : [16,20], 22 : [19,23], 23 : [20,22]};
-    const diagonalPaths = {0 : [4], 1 : [3], 2 : [6,8], 3 : [1,7,9], 4 : [0,8,10], 5 : [1,9,11], 6 : [2,13], 7 : [3,12,14],
-       8 : [2,4,13,15], 9 : [3,5,14,16], 10 : [4,15,17], 11 : [5,16], 12 : [7,18], 13 : [6,8,19],
-       14 : [7,9,18,20], 15 : [8,10,19,21], 16 : [9,11,20], 17 : [10,21], 18 : [14,22], 19 : [13,15,23], 20 : [14,16,22],
-       21 : [15,17,23], 22 : [18,20], 23 : [19,21]};
-    const scubaDiversPaths = {0 : [8], 1 : [9], 2 : [4,13], 3 : [5,14], 4 : [2,15], 5 : [3,16], 6 : [8], 7 : [9,18], 8 : [0,6,11,19],
-       9 : [1,7,11,20], 10 : [8,21], 11 : [9], 12 : [14], 13 : [2,15], 14 : [3,12,16,22], 15 : [4,13,17,23], 16 : [5,14], 17 : [15],
-       18 : [7,20], 19 : [8,21], 20 : [9,18], 21 : [10,19], 22 : [14], 23 : [15]};
-
-    const gameSteps = ["init", "startTurn", "playerActionOne", "playerActionTwo", "playerActionThree", "playerPickACard", "floodRise", "endTurn", "final"];
-
-    var playerCardsLeap = [
-    "crystal","crystal","crystal","crystal","crystal",
-    "cup","cup","cup","cup","cup",
-    "statue","statue","statue","statue","statue",
-    "sceptre","sceptre","sceptre","sceptre","sceptre",
-    "helicopter","helicopter","helicopter",
-    "sandBag","sandBag",
-    "floodRise","floodRise","floodRise"];
-
-    var tilesCards = [
-    "helipad",
-    "doorBlack","doorYellow","doorGreen","doorRed","doorWhite",
-    "temple0101","temple0102","temple0201","temple0202",
-    "temple0301","templ0302","temple0401","temple0402",
-    "desert01","desert02","desert03","coast01","coast02",
-    "coast03","swamp01","swamp02","swamp03","swamp04"];
 
     var tiles = riseTheIsland();
 
@@ -215,15 +236,15 @@ function riseTheIsland(){
     var tile13 = new Tile("temple0401", 0, false, false, "", "04", null, "#bdc3c7", "", "TPL0401");
     var tile14 = new Tile("temple0402", 0, false, false, "", "04", null, "#bdc3c7", "", "TPL0402");
     var tile15 = new Tile("coast01", 0, false, false, "", "", null, "#825a2c", "", "");
-    var tile16 = new Tile("coast02", 0, false, false, "", "", null, "#825a2c", "", "");
+    var tile16 = new Tile("coast02", 0, false, false, "", "", ["Red","Yellow"], "#825a2c", "", "");
     var tile17 = new Tile("coast03", 0, false, false, "", "", null, "#825a2c", "", "");
     var tile18 = new Tile("desert01", 0, false, false, "", "", null, "#ffd480", "", "");
     var tile19 = new Tile("desert02", 0, false, false, "", "", null, "#ffd480", "", "");
     var tile20 = new Tile("desert03", 0, false, false, "", "", null, "#ffd480", "", "");
     var tile21 = new Tile("swamp01", 0, false, false, "", "", null, "#bcf0d2", "", "");
-    var tile22 = new Tile("swamp02", 0, false, false, "", "", null, "#bcf0d2", "", "");
+    var tile22 = new Tile("swamp02", 0, false, false, "", "", ["Blue","White", "Black"], "#bcf0d2", "", "");
     var tile23 = new Tile("swamp03", 0, false, false, "", "", null, "#bcf0d2", "", "");
-    var tile24 = new Tile("swamp04", 0, false, false, "", "", null, "#bcf0d2", "", "");
+    var tile24 = new Tile("swamp04", 0, false, false, "", "", ["Red","Yellow","Green","Blue"], "#bcf0d2", "", "");
     // create a 24 array
     var tiles = new Array(tile01,tile02,tile03,tile04,tile05,tile06,tile07,tile08,tile09,tile10,
       tile11,tile12,tile13,tile14,tile15,tile16,tile17,tile18,tile19,tile20,
@@ -231,8 +252,8 @@ function riseTheIsland(){
     // shuffleIt
     tiles = shuffleArray(tiles);
     // foreach tile : make its index position
-    for(var i = 0; i<24; i++){
-      var tile = tiles[i];
+    for(let i = 0; i<24; i++){
+      let tile = tiles[i];
       tile.position = i;
       tiles[i] = tile;
     }
