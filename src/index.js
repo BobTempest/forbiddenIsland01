ff006150329abe346d42d2d49d61d1ae265eab3b
vincenal <vincenal@microsoft.com>
2018-07-17 13:20:42 +0200

    fix crash on end msg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import './strings.js';
+
 import {stringsCatalog} from './strings.js';
 
 const playerTypes = [
@@ -307,7 +307,7 @@ class Board extends React.Component {
     var floodCardsLeap = generateFloodCardsLeap();
     var floodCardsDiscard = [];
     var floodMeter = new FloodMeter(props.difficultyLevel);
-    // let mainUserMessage = new UserMessage("Welcome new Player. Choose a first action for the first character.", false, []);
+
     let mainUserMessage = new UserMessage("... INITIALIZATION... ", null, false, []);
 
     // generer les joueurs
@@ -985,7 +985,7 @@ class Board extends React.Component {
 
     if (victory === true){
         this.setState({
-            mainUserMessage: lng.youWonMsg.format(this.state.nbrOfPlayers),
+            mainUserMessage: new UserMessage(null, lng.youWonMsg.format(this.state.nbrOfPlayers), false, []),
             gameIsOver: true,
             gameIsWon: true,
             endMessage: lng.youWonMsg.format(this.state.nbrOfPlayers)});
@@ -2669,13 +2669,6 @@ handleTileClick(i) {
               }
           </div>
         </div>
-      </div>
-      <div>
-            SVG attempts :<br/>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="22" viewBox="0 0 384 512">
-              <path d="M120 72c0-39.765 32.235-72 72-72s72 32.235 72 72c0 39.764-32.235 72-72 72s-72-32.236-72-72zm254.627 1.373c-12.496-12.497-32.758-12.497-45.254 0L242.745 160H141.254L54.627 73.373c-12.496-12.497-32.758-12.497-45.254 0-12.497 12.497-12.497 32.758 0 45.255L104 213.254V480c0 17.673 14.327 32 32 32h16c17.673 0 32-14.327 32-32V368h16v112c0 17.673 14.327 32 32 32h16c17.673 0 32-14.327 32-32V213.254l94.627-94.627c12.497-12.497 12.497-32.757 0-45.254z"
-              style={{stroke: '#000000', fill: '#00cc00'}} className='blink'/>
-            </svg>
       </div>
     </div>
     );
