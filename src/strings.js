bf950830369149e04748bc7307feea9a399ac6e9
Vincent Alaphilippe <vincenal@microsoft.com>
2019-02-26 13:28:20 +0100

    card styling

--- src/strings.js
+++ src/strings.js
@@ -35,6 +35,8 @@ export const stringsCatalog = {
    ca_helicopter: "Helicopter",
    ca_sandBag: "Sand Bag",
    ca_floodRise: "Flood",
+   ca_help_helicopter: "Helicopter card : Play it whenever you want. Move one or more explorers where you want.",
+   ca_help_sandBag: "Sand bag card : Play it whenever you want. Dry any tile you want.",
    //
    fullFormattedNameAndRoleDiver: "Gina the {0}diver{1}",
    fullFormattedNameAndRolePilot: "Bob the {0}pilot{1}",
@@ -226,6 +228,8 @@ export const stringsCatalog = {
    ca_helicopter: "Hélicoptère",
    ca_sandBag: "Sac de sable",
    ca_floodRise: "Inondation",
+   ca_help_helicopter: "Carte Hélicoptère : Jouez la quand vous voulez. Déplacez un ou tous les explorateurs de la case où vous voulez.",
+   ca_help_sandBag: "Carte Sac de sable : Jouez la quand vous voulez. Asséchez une case de votre choix.",
    //
    fullFormattedNameAndRoleDiver: "Gina la {0}plongeuse{1}",
    fullFormattedNameAndRolePilot: "Bob le {0}pilote{1}",
